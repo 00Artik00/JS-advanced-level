9a449abedcd37ff41b370e1dcf7c9088fd9f0d42
Artik00 <laptenko_artem@mail.ru>
2022-05-26 11:25:55 +0300

    Descripe function in main.js

--- lesson-2/optional/main.js
+++ lesson-2/optional/main.js
@@ -45,40 +45,71 @@ class Hamburger {
         this.stuffing = this.getStuffingValue(stuffing);
         this.topping = [];
     }
+    /**
+     * Метод возвращает объект с нужным значениям цены и коллорий
+     * @param {string} size Размер бургера
+     * @returns {object} - Объект со значениями цены и коллорий для данного размера
+     */
     getSizeValue(size) {
         let result = possibleSize.find(item => item.size === size);
         return result;
     }
+    /**
+     * 
+     * @param {string} stuffing Начинка бургера
+     * @returns {object} Возвращает объект со значениям цены и коллорий для данной
+     * начинки
+     */
     getStuffingValue(stuffing) {
         let result = possibleStuffing.find(item => item.name === stuffing);
         return result;
     }
-
+    /**
+     * Метод добавляет добавку в массив добавок this.topping
+     * @param {string} topping Добавка которую необходимо добавить
+     */
     addTopping(topping) {
         let top = possibleTopping.find(item => item.name === topping);
         this.topping.push(top);
         console.log(this);
-    } // Добавить добавку }
+    }
+    /**
+     * Метод удаляет добавку из массива добавок this.topping
+     * @param {string} topping Добавка, которую необходимо удалить
+     */
     removeTopping(topping) {
-        let index = this.topping.indexOf(possibleTopping.find(item => item.name === topping));
+        let index = this.topping.indexOf(possibleTopping
+            .find(item => item.name === topping));
         if (index > -1) {
             this.topping.splice(index, 1);
         }
         console.log(this);
-    } // Убрать добавку }
+    }
+    /**
+     * Метод выводит в консоль список текущих добавок
+     */
     getToppings() {
         let topings = [];
         for (const top of this.topping) {
             topings.push(top.name);
         }
         console.log(`Список текущих добавок: ${topings}`);
-    } // Получить список добавок }
+    }
+    /**
+     * Метод выводит в консоль текущий размер бургера
+     */
     getSize() {
         console.log(`Размер гамбургера: ${this.size.size}`);
-    } // Узнать размер гамбургера }
+    }
+    /**
+     * Метод выводит в консоль текущую начинку бургера
+     */
     getStuffing() {
         console.log(`Начинка гамбургера: ${this.stuffing.name}`);
-    } // Узнать начинку гамбургера }
+    }
+    /**
+     * Метод рассчитывает текущую стоимость бургера
+     */
     calculatePrice() {
         const sizePrice = this.size.cost;
         const stuffingPrice = this.stuffing.cost;
@@ -86,8 +117,12 @@ class Hamburger {
         for (const top of this.topping) {
             toppingPrice += top.cost;
         }
-        console.log(`Цена за данный бургер составила: ${sizePrice + stuffingPrice + toppingPrice}`);
-    } // Узнать цену }
+        console.log(`Цена за данный бургер составила: \
+${sizePrice + stuffingPrice + toppingPrice} рублей`);
+    }
+    /**
+     * Метод рассчитывает текущую каллорийность бургера
+     */
     calculateCalories() {
         const sizeCal = this.size.callories;
         const stuffingCal = this.stuffing.callories;
@@ -95,7 +130,18 @@ class Hamburger {
         for (const top of this.topping) {
             toppingCal += top.callories;
         }
-        console.log(`Коллорийность данного бургера: ${sizeCal + stuffingCal + toppingCal}`);
-    } // Узнать калорийность }
+        console.log(`Каллорийность данного бургера: \
+${sizeCal + stuffingCal + toppingCal}`);
+    } 
 }
 const hamburg = new Hamburger('big', 'cheese');
+hamburg.addTopping('spice');
+hamburg.addTopping('mayonnaise');
+hamburg.removeTopping('spice');
+hamburg.getToppings();
+hamburg.getSize();
+hamburg.getStuffing();
+hamburg.calculatePrice();
+hamburg.calculateCalories();
+
+
